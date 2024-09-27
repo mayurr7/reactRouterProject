47c0f92ed742684518fd9c291f9b4af94b6f9009
Mayur Tekale <mayurtekale7@gmail.com>
2024-09-27 18:00:43 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from "react"
+import { useState } from 'react'
 
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className="bg-gray-600">React Router</h1>
+     
+      
     </>
   )
 }
