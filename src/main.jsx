dc133c856b510769b60e61bb026f6e9f2c8e9e1f
Mayur Tekale <mayurtekale7@gmail.com>
2024-09-27 18:01:06 +0530

    done header footer and home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
 import './index.css'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx';
+
+ const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: '',
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      }
+    ]
+  }
+ ]
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+  
   </StrictMode>,
 )
